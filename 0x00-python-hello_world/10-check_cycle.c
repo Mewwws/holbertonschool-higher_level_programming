2a55b31a5862eb5d0ef22bb406d1ddac8ee4e3de
Mewws <mehdi.boulifa@yahoo.fr>
2021-04-26 16:55:21 +0100

    python

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * check_cycle - checks if the linked list cycles
+ * @list: checked list
+ * Return: 1 if cycle 0 if not
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *check = list;
+	listint_t *checc = list->next;
+
+	if (check == NULL)
+		return (0);
+	while (check->next)
+	{
+		check = check->next;
+		checc = checc->next->next;
+		if (check == checc)
+			return (1);
+	}
+	return (0);
+}
