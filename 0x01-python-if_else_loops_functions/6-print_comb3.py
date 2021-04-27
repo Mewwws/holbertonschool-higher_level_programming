467ff2865ae46c0e9bedb799c453b0a4a6072bf2
Mewws <mehdi.boulifa@yahoo.fr>
2021-04-27 18:09:09 +0100

    py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 for i in range(0, 9):
     for j in range(i + 1, 10):
-        print(i, j, sep='', end='')
+        print("{}{}".format(i, j), end='')
         if i < 8 or j < 9:
             print(", ", end='')
         else:
             print()
-
--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def islower(c):
-    return c > 96 & c < 123
+    return (c > 96 and c < 123)
