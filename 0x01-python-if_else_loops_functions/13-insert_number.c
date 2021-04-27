bcc6bac536e1d5f64abf125683e4b7690c080f85
Mewws <mehdi.boulifa@yahoo.fr>
2021-04-27 23:02:35 +0100

    py

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,10 +1,12 @@
 #inclde "lists.h"
 
 /**
- *
- *
+ * insert_node - does something
+ * @head: where brain is
+ * @number: counting tool
+ * Return: some head
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-	
+	return (*head);
 }
