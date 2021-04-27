018b0f0e9b004dd44ef4ad4ca39a9b5440a2d932
Mewws <mehdi.boulifa@yahoo.fr>
2021-04-27 14:20:36 +0100

    uu

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-for i in range(0, 8):
+for i in range(0, 9):
     for j in range(i + 1, 10):
-        if i == 8 & j == 9:
-            break
-        print(i, j,", ", sep='', end='')
-i = 8
-print(i, j, sep='')
+        print(i, j, sep='', end='')
+        if i < 8 or j < 9:
+            print(", ", end='')
+print()
