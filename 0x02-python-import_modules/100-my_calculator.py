e46cd21efd4c33de6f8aba8929568de4c2ccbba8
Mewws <mehdi.boulifa@yahoo.fr>
2021-04-29 21:37:56 +0100

    py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,9 +2,7 @@
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     import sys
-    if not len(sys.argv) == 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        quit(1)
+
     else:
         a = int(sys.argv[1])
         b = int(sys.argv[3])
