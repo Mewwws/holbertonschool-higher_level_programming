3df9a94af612e686de19e01353879e848ff4c2ef
Mewws <mehdi.boulifa@yahoo.fr>
2021-04-29 21:42:49 +0100

    py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,14 +5,15 @@ if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    s = sys.argv[2]
-    if s == '+':
-        print("{} {} {} = {}".format(a, s, b, add(a, b)))
-    if s == '-':
-        print("{} {} {} = {}".format(a, s, b, sub(a, b)))
-    if s == '*':
-        print("{} {} {} = {}".format(a, s, b, mul(a, b)))
-    if s == '/':
-        print("{} {} {} = {}".format(a, s, b, div(a, b)))
+    else:
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+        s = sys.argv[2]
+        if s == '+':
+            print("{} {} {} = {}".format(a, s, b, add(a, b)))
+        if s == '-':
+            print("{} {} {} = {}".format(a, s, b, sub(a, b)))
+        if s == '*':
+            print("{} {} {} = {}".format(a, s, b, mul(a, b)))
+        if s == '/':
+            print("{} {} {} = {}".format(a, s, b, div(a, b)))
