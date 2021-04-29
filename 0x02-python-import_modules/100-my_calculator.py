4cedc24ba38d0f88dd896603ad527c36c131b7d7
Mewws <mehdi.boulifa@yahoo.fr>
2021-04-29 21:36:43 +0100

    py

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    import sys
+    if not len(sys.argv) == 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        quit(1)
+    else:
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+        s = sys.argv[2]
+    if s == '+':
+        print("{} {} {} = {}".format(a, s, b, add(a, b)))
+    if s == '-':
+        print("{} {} {} = {}".format(a, s, b, sub(a, b)))
+    if s == '*':
+        print("{} {} {} = {}".format(a, s, b, mul(a, b)))
+    if s == '/':
+        print("{} {} {} = {}".format(a, s, b, div(a, b)))
