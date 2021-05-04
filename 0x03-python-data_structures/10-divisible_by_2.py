fde9127aa12d2d87766f292afa65c6fd2121b223
Mewws <mehdi.boulifa@yahoo.fr>
2021-05-04 23:24:06 +0100

    py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     check = []
-    for i in range(len(my_list)):
-        if my_list[i] % 2:
-            check.append(true)
+    for i in my_list:
+        if i % 2:
+            check = check + true
         else:
-            check.append(false)
+            check =+ false
+    return check
