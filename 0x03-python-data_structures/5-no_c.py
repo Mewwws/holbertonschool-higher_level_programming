55a769712e0d8db772f3eac34e4b739650e6c43b
Mewws <mehdi.boulifa@yahoo.fr>
2021-05-04 20:28:52 +0100

    py

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    if my_string is not None:
+        j = 0
+        for i in my_string:
+            if i is not c or C:
+                new = new + i
+        return (new)
