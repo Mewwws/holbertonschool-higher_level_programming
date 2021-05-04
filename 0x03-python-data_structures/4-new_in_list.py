0babecf4def3b3e23646a8d27e7bfdb6fc8af3aa
Mewws <mehdi.boulifa@yahoo.fr>
2021-05-04 20:09:09 +0100

    py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,6 +2,6 @@
 def replace_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
         return (my_list)
-    newlist = my_list
+    newlist = my_list[]
     newlist[idx] = element
     return(newlist)
