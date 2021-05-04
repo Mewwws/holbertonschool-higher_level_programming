e08821761ba0dd9edd3458c09e39c7906de90846
Mewws <mehdi.boulifa@yahoo.fr>
2021-05-04 23:20:03 +0100

    py

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    check = []
+    for i in my_list:
+        if i % 2:
+            check = check + true
+        else:
+            check =+ false
