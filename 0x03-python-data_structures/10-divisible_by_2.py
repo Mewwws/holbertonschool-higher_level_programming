f6ab14ec9cf9684a30da2d5a935c73ba610ef4d8
Mewws <mehdi.boulifa@yahoo.fr>
2021-05-04 23:21:00 +0100

    py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,6 +3,6 @@ def divisible_by_2(my_list=[]):
     check = []
     for i in my_list:
         if i % 2:
-            check = check + true
+            check.append(true)
         else:
-            check =+ false
+            check.append(false)
