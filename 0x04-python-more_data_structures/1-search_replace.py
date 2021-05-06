e7eb517624283dca13c2a570844ea6484d817638
Mewws <mehdi.boulifa@yahoo.fr>
2021-05-06 22:07:36 +0100

    py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    mat = []
+    rep = []
     for i in len(my_list):
-        if my_list[i] == search:
-            my_list[i] = replace
-            
+        if i == search:
+            rep.append(replace)
+        else:
+            rep.append(i)
+    return (rep)
