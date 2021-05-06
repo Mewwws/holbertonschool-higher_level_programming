2f9c76737dc48ad249d4ac69018c2086756ddeb0
Mewws <mehdi.boulifa@yahoo.fr>
2021-05-06 22:08:28 +0100

    py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     rep = []
-    for i in len(my_list):
+    for i in my_list:
         if i == search:
             rep.append(replace)
         else:
