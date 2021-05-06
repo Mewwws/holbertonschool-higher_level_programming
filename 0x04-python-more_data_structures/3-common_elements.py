6d27beb112b307bb261f7de94535ec17a2df3425
Mewws <mehdi.boulifa@yahoo.fr>
2021-05-06 23:07:06 +0100

    py

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
-def only_diff_elements(set_1, set_2):
-    return (set_1 & set_2)
+def common_elements(set_1, set_2):
+    dup = []
+    for i in set_1:
+        for j in set_2:
+            if i == j:
+                dup = dup + i
+    return (dup)
